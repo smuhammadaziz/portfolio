2ba4bfee6b38d2867f05686ac82633785508ea7a
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-11-16 21:36:34 +0500

    add: adding some changes

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import mylogo from '../../assets/logo.png';
+import React from "react";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import mylogo from "../../assets/rasm.png";
 
 function Intro() {
   return (
-    <div className='bg-slate-100 py-10 lg:py-20'>
-      <div className='container mx-auto px-6 md:px-20 lg:px-40 flex flex-col md:flex-row items-center justify-between'>
-        {/* Image Section */}
-        <div className='flex-1 flex justify-center md:justify-end mt-10 md:mt-0 order-1 md:order-2'>
-          <img src={mylogo} alt="Logo" className='w-64 md:w-80 lg:w-5/6' />
-        </div>
+    <div className="bg-slate-100">
+      <div className="container mx-auto px-6 md:px-40 flex justify-between items-center">
+        {/* Wrapper */}
+        <div className="p-10 md:p-16 lg:p-20 flex flex-col items-center text-center space-y-8">
+          {/* Image Section */}
+          <img
+            src={mylogo}
+            alt="Logo"
+            className="w-48 md:w-60 lg:w-72 rounded-full border-4 shadow-lg"
+          />
 
-        {/* Text Section */}
-        <div className='flex-1 text-center md:text-left order-2 md:order-1'>
-          <h1 className='text-4xl md:text-5xl lg:text-6xl text-black font-bold font-mono'>
-            Muhammadaziz Ravshanbekov
-          </h1>
-          <h2 className='text-lg md:text-xl mt-4 md:mt-2'>Software Engineer @ Google</h2>
-          <p className='text-base md:text-lg mt-4'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, aliquid! Numquam atque a sequi praesentium
-            eos voluptates cupiditate blanditiis, ullam assumenda cum quis exercitationem sapiente sit consectetur fuga
-            nam recusandae.
-          </p>
-          <button className="border-indigo-950 bg-indigo-900 hover:bg-transparent hover:text-indigo-950 text-white rounded-lg border-2 px-8 md:px-12 py-3 text-lg mt-7">
+          {/* Text Section */}
+          <div>
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-gray-800 font-roboto">
+              Muhammadaziz Ravshanbekov
+            </h1>
+            <h2 className="text-lg md:text-xl lg:text-2xl text-gray-600 mt-2 font-roboto">
+              Software Engineer @KSB-SOFT
+            </h2>
+            <p className="text-base md:text-lg lg:text-xl text-gray-500 mt-4 leading-relaxed font-roboto">
+              Passionate about building scalable software and elegant
+              user-centric experiences. Let’s solve complex problems together!
+            </p>
+          </div>
+
+          {/* Button */}
+          <button className="bg-indigo-900 text-white px-8 md:px-12 py-3 rounded-full text-lg md:text-xl shadow-lg hover:bg-indigo-700 transition transform hover:scale-105 font-roboto">
             About Me
           </button>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./public/index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"], // Set Roboto as the default sans font
+      },
+    },
   },
   plugins: [],
 };
