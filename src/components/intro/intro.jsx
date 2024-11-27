f979fb08e09dafc39fba52c11760562c8e989619
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-11-28 00:20:20 +0500

    add: adding some changes

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -20,7 +20,6 @@ function Intro() {
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1, delay: 0.5 }}
         />
-        {/* Text Section */}
         <motion.div
           initial={{ opacity: 0, x: -50 }}
           animate={{ opacity: 1, x: 0 }}
