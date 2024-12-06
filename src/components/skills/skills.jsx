7fd517d5fc4d83dee97fd237337acca837bf0733
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-12-06 23:33:18 +0500

    add: adding some changes for skills

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -232,8 +232,8 @@ function SkillsSection() {
   };
 
   return (
-    <section className="relative py-20 overflow-hidden bg-white">
-      {/* <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f46e5_1px,transparent_1px),linear-gradient(to_bottom,#4f46e5_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] opacity-[0.1]" /> */}
+    <section className="relative py-20 overflow-hidden bg-gradient-to-b from-gray-50 to-white">
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f46e5_1px,transparent_1px),linear-gradient(to_bottom,#4f46e5_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_30%_at_50%_0%,#000_70%,transparent_100%)] opacity-[0.1]" />
 
       <div className="container mx-auto px-6 md:px-40 relative z-10">
         <div className="text-center max-w-3xl mx-auto mb-16">
